167f704ce73258217ed505a4c22f65210b6c23e1
MoonWatcher582 <erbronne@gmail.com>
2015-09-21 15:37:10 -0400

    Added file output for failures and successes

--- req.rb
+++ req.rb
@@ -1,8 +1,25 @@
 require 'net/http'
 
 uri = URI('http://www.omdbapi.com/')
-params = { :t => 'Pulp Fiction' }
-uri.query = URI.encode_www_form(params)
+films = ['Pulp Fiction', 'Cowboy Bebop', 'Metal Gear Solid 3: Subsistence']
 
-res = Net::HTTP.get_response(uri)
-puts res.body if res.is_a?(Net::HTTPSuccess)
+fsuccess = "suc.txt"
+ffail = "fail.txt"
+
+fs = File.open(fsuccess, 'w')
+ff = File.open(ffail, 'w')
+
+films.each do |film|
+	params = { :t => film }
+	uri.query = URI.encode_www_form(params)
+	res = Net::HTTP.get_response(uri)
+
+	if res.is_a?(Net::HTTPSuccess)
+		fs.puts res.body
+	else
+		ff.puts 'failed to access #{film}'
+	end
+end
+
+fs.close
+ff.close
