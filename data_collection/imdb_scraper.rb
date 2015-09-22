0989b1ad138af6fcd9c562f4ec75e492f989c1b9
MoonWatcher582 <erbronne@gmail.com>
2015-09-22 11:15:32 -0400

    Aaaaaand turns out imdb tells you the type -_-; type information deleted, type checking removed

--- data_collection/imdb_scraper.rb
+++ data_collection/imdb_scraper.rb
@@ -10,10 +10,7 @@ fs = File.open(fsuccess, 'w')
 ff = File.open(ffail, 'w')
 sf = File.open(source, 'r')
 
-sf.each_line do |line|
-	line_split = line.split(',')
-	film = line_split[0]
-	type = line_split[1].chomp
+sf.each_line do |film|
 
 	params = { :t => film }
 	uri.query = URI.encode_www_form(params)
@@ -21,20 +18,6 @@ sf.each_line do |line|
 
 	if res.is_a?(Net::HTTPSuccess)
 		fs.puts "#{film}"
-		case type
-		when "f"
-			fs.puts "\tfilm"
-		when "af"
-			fs.puts "\tanimated film"
-		when "ls"
-			fs.puts "\tlive series"
-		when "as"
-			fs.puts "\tanimated series"
-		when "vg"
-			fs.puts "\tvideo game"
-		else
-			fs.puts "\tWRONG"
-		end
 	else
 		ff.puts "failed to access #{film}"
 	end
