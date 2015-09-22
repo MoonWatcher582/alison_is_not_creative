20c28c1b9549a1619ed5810f094670e4d02ef08b
MoonWatcher582 <erbronne@gmail.com>
2015-09-22 11:11:01 -0400

    Adding a file made of (movie title, movie type) tuples; updated the scraper to serve as a spell checker (if a movie of type identifier is wrong, the output will make it clear)

--- data_collection/imdb_scraper.rb
+++ data_collection/imdb_scraper.rb
@@ -1,23 +1,42 @@
 require 'net/http'
 
 uri = URI('http://www.omdbapi.com/')
-films = ['Pulp Fiction', 'Cowboy Bebop', 'Metal Gear Solid 3: Subsistence']
 
+source = "films.txt"
 fsuccess = "suc.txt"
 ffail = "fail.txt"
 
 fs = File.open(fsuccess, 'w')
 ff = File.open(ffail, 'w')
+sf = File.open(source, 'r')
 
-films.each do |film|
+sf.each_line do |line|
+	line_split = line.split(',')
+	film = line_split[0]
+	type = line_split[1].chomp
+
 	params = { :t => film }
 	uri.query = URI.encode_www_form(params)
 	res = Net::HTTP.get_response(uri)
 
 	if res.is_a?(Net::HTTPSuccess)
-		fs.puts res.body
+		fs.puts "#{film}"
+		case type
+		when "f"
+			fs.puts "\tfilm"
+		when "af"
+			fs.puts "\tanimated film"
+		when "ls"
+			fs.puts "\tlive series"
+		when "as"
+			fs.puts "\tanimated series"
+		when "vg"
+			fs.puts "\tvideo game"
+		else
+			fs.puts "\tWRONG"
+		end
 	else
-		ff.puts 'failed to access #{film}'
+		ff.puts "failed to access #{film}"
 	end
 end
 
