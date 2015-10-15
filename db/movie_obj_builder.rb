3c3c512e53374827aa9f4732e793adb4d80b1cd0
MoonWatcher582 <erbronne@gmail.com>
2015-10-15 09:12:55 -0400

    updated class to have a generic constructor

--- db/movie_obj_builder.rb
+++ db/movie_obj_builder.rb
@@ -9,31 +9,45 @@ class Movie
 	attr_reader :poster
 	attr_reader :type
 
-	def build_from_data(line)
-		film_info = line.split(';')
-		@title = film_info[0].chomp
-		@year = film_info[1].chomp
-		@rated = film_info[2].chomp
-		@directors = film_info[4].split(', ')
-		@actors = film_info[5].split(', ')
-		@country = film_info[6].chomp
-		@poster = film_info[7].chomp
-		parse_genres(film_info[3].split(', '), film_info[8].chomp)
+	def initialize(title, year, rated, genres, directors, actors, country, poster, type)
+		@title = title
+		@year = year
+		@rated = rated
+		@genres = genres
+		@directors = directors
+		@actors = actors
+		@country = country
+		@poster = poster
+		@type = type
 	end
 
-	def parse_genres(genres, type)
+	def self.build_from_data(line)
+		film_info = line.split(';')
+		title = film_info[0].chomp
+		year = film_info[1].chomp
+		rated = film_info[2].chomp
+		directors = film_info[4].split(', ')
+		actors = film_info[5].split(', ')
+		country = film_info[6].chomp
+		poster = film_info[7].chomp
+		genres, type = parse_genres(film_info[3].split(', '), film_info[8].chomp)
+		return Movie.new(title, year, rated, genres, directors, actors, country, poster, type)
+	end
+
+	def self.parse_genres(genres, type)
 		if genres.include? 'Animation'
 			if type == 'movie'
-				@type = 'animated movie'
+				f_type = 'animated movie'
 			elsif type == 'series'
-				@type = 'animated series'
+				f_type = 'animated series'
 			else
-				@type = type
+				f_type = type
 			end
 		else
-			@type = type
+			f_type = type
 		end
 
-		@genres = genres - ['Animation']
+		f_genres = genres - ['Animation']
+		return f_genres, f_type
 	end
 end
