473339f95989439ecd90d7f4b412a43cf39cedc8
MoonWatcher582 <erbronne@gmail.com>
2015-12-01 17:26:37 -0500

    Bug fixing

--- db/db_builder_user_data.rb
+++ db/db_builder_user_data.rb
@@ -1,10 +1,10 @@
 require 'sequel'
 require 'bcrypt'
 
-if ARGV.length != 1 or ARGV[0] == '-h'
-	puts "USAGE: ruby db_builder_user_data.rb <user info file>"
+if ARGV.length != 2 or ARGV[0] == '-h'
+	puts "USAGE: ruby db_builder_user_data.rb <user info file> <ratings file>"
 	puts "Builds a database out of the user data"
-	exist 1
+	exit 1
 end
 
 # Constant strings
@@ -17,11 +17,12 @@ def insert_value(value, table, column)
 	DB.run "INSERT INTO #{table} (#{column}) VALUES (#{value})" if not element_exists? value, table, column
 end
 
-def element_exists?(value, table, colum)
+def element_exists?(value, table, column)
+	puts "SELECT * FROM #{table} WHERE #{column} = #{value.chomp}"
 	DB["SELECT * FROM #{table} WHERE #{column} = #{value.chomp}"].count > 0
 end
 
-DB = Sequel.connect('sqlite://users.db')
+DB = Sequel.connect('sqlite://movies.db')
 
 puts "Parsing and inserting from #{ARGV[0]}"
 File.open(ARGV[0], 'r') do |user_info|
